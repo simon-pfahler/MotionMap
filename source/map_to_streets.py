efc7ff3027c33a9f78effce9e44f65f5dc124110
simon-pfahler <simon-pfahler@gmx.de>
2025-05-13 22:31:23 +0200

    Identify some problems with current approach, and add cosine similarity function

--- source/map_to_streets.py
+++ source/map_to_streets.py
@@ -2,7 +2,11 @@ import networkx as nx
 
 from source.plot import *
 from source.track import *
-from source.utility import likelihood_edge, likelihoods_first_edge
+from source.utility import (
+    cosine_similarity,
+    likelihood_edge,
+    likelihoods_first_edge,
+)
 
 
 def map_track_to_street_network(track, street_network):
@@ -21,36 +25,40 @@ def map_track_to_street_network(track, street_network):
         fig, ax = plt.subplots(1, 1)
 
     for segment in range(track.segments()):
-        # list that contains the calculated likelihoods
-        # of different edges at different time points
-        L = [dict() for _ in range(track.len(segment))]
-        # list that contains the maximum likelihood in every step
+        # list that contains the calculated quality values
+        # of the possible (edge, previous_edge) pairs at all timepoints
+        # for index 0, the keys are only edges, not tuples of edges, as there
+        # is no previous edge
+        # TODO: Would it be sufficient to always use the current edge as key?
+        Q = [dict() for _ in range(track.len(segment))]
 
         # current most probable edge list
         edges = [(None, None) for _ in range(track.len(segment))]
         # index of the next edge for the currently most probable path
+        # (i.e. the first index i for which edges[i] == (None, None)
         next_index = 0
 
-        # get likelihoods for starting nodes
-        L[0] = likelihoods_first_edge(track.utm(segment, 0), street_network)
+        # get quality values for starting nodes of the track
+        Q[0] = likelihoods_first_edge(track.utm(segment, 0), street_network)
 
         # loop until we got the most probable complete path
         while next_index < track.len(segment):
-            # DEBUG
+
+            # >>> DEBUG
             if next_index > 0:
                 if next_index > 1:
                     print(
                         next_index,
-                        L[next_index - 1][
+                        Q[next_index - 1][
                             (edges[next_index - 1], edges[next_index - 2])
                         ],
-                        L[0][(518671757, 1752315539)],
+                        Q[0][(518671757, 1752315539)],
                     )
                 else:
                     print(
                         next_index,
-                        L[next_index - 1][edges[next_index - 1]],
-                        L[0][(518671757, 1752315539)],
+                        Q[next_index - 1][edges[next_index - 1]],
+                        Q[0][(518671757, 1752315539)],
                     )
                 # build the graph from the edges
                 if debug:
@@ -81,78 +89,103 @@ def map_track_to_street_network(track, street_network):
                     ax.set_ylim(min(y0, y1) - 100, max(y0, y1) + 100)
                     plt.show()
                     plt.pause(0.05)
-            # DEBUG
+            # <<< DEBUG
 
-            # get relevant likelihoods for the next step and proposed edge
-            next_edge = (None, None)
+            # special case if next_index == 0
             if next_index == 0:
-                next_edge = max(L[0], key=L[0].get)
+                next_edge = max(Q[0], key=Q[0].get)
                 # go one step forward
                 edges[next_index] = next_edge
                 next_index += 1
                 continue
 
+            # TODO: Check here if we should take a step backwards
+
+            # proposal for next edge
+            next_edge = (None, None)
+
             # shorthand for current and previous edge
             curr_edge = edges[next_index - 1]
             prev_edge = edges[next_index - 2]
 
-            # get relevant edges
+            # the possible next edges are the current edge and the neighboring
+            # edges at both ends of the current edge
             relevant_edges = (
-                [edges[next_index - 1]]
-                + list(street_network.graph.edges(edges[next_index - 1][0]))
-                + list(street_network.graph.edges(edges[next_index - 1][1]))
+                [curr_edge]
+                + list(street_network.graph.edges(curr_edge[0]))
+                + list(street_network.graph.edges(curr_edge[1]))
             )
-            # store their likelihoods in L
-            max_likelihood = 0
-            for edge in relevant_edges:
-                if (edge, curr_edge) not in L[next_index].keys():
-                    L[next_index][(edge, curr_edge)] = likelihood_edge(
-                        track.utm(segment, next_index),
-                        street_network.utm(edge[0]),
-                        street_network.utm(edge[1]),
-                    )
-                if L[next_index][(edge, curr_edge)] > max_likelihood:
-                    next_edge = edge
-                    max_likelihood = L[next_index][(edge, curr_edge)]
 
+            # add their quality values to Q[next_index]
+            max_likelihood = -np.inf
+            for edge in relevant_edges:
+                if (edge, curr_edge) not in Q[next_index].keys():
+                    track_edge_start = track.utm(segment, next_index - 1)
+                    track_edge_end = track.utm(segment, next_index)
+                    track_vec = np.array(track_edge_end) - np.array(
+                        track_edge_start
+                    )
+                    if edge[0] in curr_edge:
+                        street_edge_start = street_network.utm(edge[0])
+                        street_edge_end = street_network.utm(edge[1])
+                    else:
+                        street_edge_start = street_network.utm(edge[1])
+                        street_edge_end = street_network.utm(edge[0])
+                    street_vec = np.array(street_edge_end) - np.array(
+                        street_edge_start
+                    )
+                    Q[next_index][(edge, curr_edge)] = likelihood_edge(
+                        track_edge_end,
+                        street_edge_start,
+                        street_edge_end,
+                    )
+                if Q[next_index][(edge, curr_edge)] > max_likelihood:
+                    next_edge = edge
+                    max_likelihood = Q[next_index][(edge, curr_edge)]
+
+            # if no possible edge could be found, go back one step
+            # TODO: Is this even possible?
             if next_edge == (None, None):
-                L[next_index - 1][(curr_edge, edges[next_index - 2])] = 0
+                Q[next_index - 1][(curr_edge, edges[next_index - 2])] = 0
                 edges[next_index - 1] = (None, None)
                 next_index -= 1
                 continue
 
-            if next_index > 1:
-                L[next_index - 1][(curr_edge, edges[next_index - 2])] *= L[
-                    next_index
-                ][(next_edge, curr_edge)]
+            # update the Q values of the previous step
+            Q_change = Q[next_index][(next_edge, curr_edge)]
+            if next_index == 1:
+                Q[0][curr_edge] += Q_change
             else:
-                L[next_index - 1][curr_edge] *= L[next_index][
-                    (next_edge, curr_edge)
-                ]
-            L[next_index][(next_edge, curr_edge)] = 1
+                Q[next_index - 1][
+                    (curr_edge, edges[next_index - 2])
+                ] += Q_change
+            for edge in relevant_edges:
+                Q[next_index][(edge, curr_edge)] -= Q_change
 
-            # go back one step if last step is now non-optimal
+            # check if there is now an edge at time next_index-1 that has a
+            # higher Q value
             go_back = False
-            relevant_prev_edges = list()
-            if next_index > 1:
+            if next_index == 1:
+                relevant_prev_edges = list(Q[0].keys())
+                for edge in list(Q[0].keys()):
+                    if Q[0][edge] > Q[0][curr_edge]:
+                        go_back = True
+                        break
+            else:
+                # relevant_prev_edges are defined analogously to relevant_edges
                 relevant_prev_edges = (
-                    [edges[next_index - 2]]
+                    [prev_edge]
                     + list(street_network.graph.edges(prev_edge[0]))
                     + list(street_network.graph.edges(prev_edge[1]))
                 )
                 for edge in relevant_prev_edges:
                     if (
-                        L[next_index - 1][(edge, prev_edge)]
-                        > L[next_index - 1][(curr_edge, prev_edge)]
+                        Q[next_index - 1][(edge, prev_edge)]
+                        > Q[next_index - 1][(curr_edge, prev_edge)]
                     ):
                         go_back = True
-                        break
-            else:
-                relevant_prev_edges = list(L[0].keys())
-                for edge in relevant_prev_edges:
-                    if L[next_index - 1][edge] > L[next_index - 1][curr_edge]:
-                        go_back = True
-                        break
+
+            # go back one step if last step is now non-optimal
             if go_back:
                 edges[next_index - 1] = (None, None)
                 next_index -= 1
--- source/utility.py
+++ source/utility.py
@@ -1,6 +1,17 @@
 import numpy as np
 
 
+def cosine_similarity(vec1, vec2):
+    """
+    Get the cosine similarity of two vectors
+
+    :param vec1: UTM coordinates of the first vector
+    :param vec2: UTM coordinates of the second vector
+    """
+
+    return np.dot(vec1, vec2) / np.linalg.norm(vec1) / np.linalg.norm(vec2)
+
+
 def min_distance_point_edge(point, edge_start, edge_end):
     """
     Get the minimal distance between a point and an edge
@@ -41,7 +52,7 @@ def likelihood_edge(point, edge_start, edge_end):
     :param edge_end: UTM coordinates of the edge end
     """
 
-    return np.exp(-min_distance_point_edge(point, edge_start, edge_end) / 5)
+    return -min_distance_point_edge(point, edge_start, edge_end) / 5
 
 
 def likelihoods_first_edge(point, street_network):
